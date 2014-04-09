74251f0fcbe72b19fd674692390b3c0e27b79776
Gary Riger <grigerny@aol.com>
2014-04-09 19:28:43 -0400

    added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Networkingapp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { :host => 'http://mighty-hollows-6002.herokuapp.com/' }
 end
