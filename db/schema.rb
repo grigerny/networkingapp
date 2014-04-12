2f831d0ef828d54dfa38b5e5e8a89cdeb8958a2f
Gary Riger <grigerny@aol.com>
2014-04-12 16:57:08 -0400

    added industry to user and fixed wizard form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140410202738) do
+ActiveRecord::Schema.define(version: 20140412042352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20140410202738) do
     t.string   "city"
     t.string   "state"
     t.string   "zip"
+    t.string   "title"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
