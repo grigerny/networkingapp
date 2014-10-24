d0f4daf383ae6e46548e1e4c8c7d21a0b44dd7b4
Gary Riger <grigerny@aol.com>
2014-10-24 10:59:13 -0400

    added description to group

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140905170643) do
+ActiveRecord::Schema.define(version: 20141024144229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140905170643) do
     t.string   "address"
     t.string   "venue"
     t.string   "time"
+    t.string   "description"
   end
 
   create_table "memberships", force: true do |t|
